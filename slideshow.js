19503786dacb79e705d78abd6719a1797756c458
probabl <71607095+probabl@users.noreply.github.com>
2024-06-09 15:16:35 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -27,6 +27,6 @@ function show_Slides(slide_To_Show){
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slide_Index - 1].style.display = "block";
+  dots[slide_Index - 1].className += " active";
 }
