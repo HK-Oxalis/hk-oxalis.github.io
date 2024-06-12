0f3a025e1f5e4008d93c052ad35be87ad840cf28
probabl <71607095+probabl@users.noreply.github.com>
2024-06-12 13:25:30 +0100

    Create dithering.js

---
+++ P5js/dithering.js
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="P5js/p5.js"></script>
+  <title>Image Dithering</title>
+  
+</head>
+<body>
+  <header>
+    <h1>
+      Dithering About
+    </h1>  
+  </header>
+
+  <script src="P5js/dither_Sketch.js"></script>
+
+  
+</body>
+</html>
