8fd34ffdd2ed2cb124d1f9a262b794baa10d6436
probabl <71607095+probabl@users.noreply.github.com>
2024-06-13 11:39:00 +0100

    Update dither_Sketch.js

--- P5js/dither_Sketch.js
+++ P5js/dither_Sketch.js
@@ -4,7 +4,7 @@ let colourFactor = 1;
 let ditherpattern;
 
 function Change_Image() {
-  let userFile = document.getElementById("image-input").value;
+  let userFile = document.getElementById("image-input");
   //userFile = userFile.replace("C:\\fakepath\\", "");
   console.log(userFile);
   //let userFileURL = URL.createObjectURL(userFile);
