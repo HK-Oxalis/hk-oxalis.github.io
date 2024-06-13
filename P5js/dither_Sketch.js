5446b7d61918313c724b0e1c788bb6ec34768c77
probabl <71607095+probabl@users.noreply.github.com>
2024-06-13 12:46:08 +0100

    Update dither_Sketch.js

--- P5js/dither_Sketch.js
+++ P5js/dither_Sketch.js
@@ -20,7 +20,7 @@ function Image_Error(){
 
 function preload(){
   exampleShader = loadShader("P5js/example.vert", "P5js/dither.frag");
-  backgroundImage = loadImage("Images/betta2_dithered.png");
+  backgroundImage = loadImage("Images/jade_hare_photo.png");
   dither_Pattern = loadImage("P5js/Dither_pattern.png");
 }
 
